a3434fc4756bb19d886fcdc83a17e61f32effcb8
Shubham Sharma <87312848+shubhams-git@users.noreply.github.com>
2024-10-10 03:48:24 +1100

    Added Loadable feature

--- recoil_advanced/src/App.jsx
+++ recoil_advanced/src/App.jsx
@@ -1,32 +1,32 @@
-import { atomFamily, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil"
+import { atomFamily, useRecoilState, useRecoilValue, useRecoilValueLoadable, useSetRecoilState } from "recoil"
 import { allNotifsAtom, totalNotificationsSelector } from "./assets/store/atoms/atoms"
 import { todoFamily } from "./assets/store/atom-family/todoFamily"
 export default function App(){
   
   const setTodo = useSetRecoilState(todoFamily(2))
 
-  setTimeout(()=>{
-    console.log("Timeout triggered")
-    setTodo((todo)=>{
-      todo.title = "Hi",
-      todo.description = "Hey"
-    })
-  },1000)
-
   return <>
     <Todo id={1} />
+    <Todo id={2} />
     <Todo id={3} />
-    <Todo id={3} />
-    <Todo id={3} />
+    <Todo id={4} />
   </>
 }
 
-
 function Todo({id}){
-  const todo = useRecoilValue(todoFamily(id))
-  return <>
-    <b>{todo.title}</b>
-    <p>{todo.description}</p>
+  const todo = useRecoilValueLoadable(todoFamily(id))
+  if(todo.state=="loading"){
+    return <>
+    <b>Loading...</b>
+  </>
+    
+  }else if(todo.state=="hasValue"){
+    return <>
+    <b>{todo.contents.title}</b>
+    <p>{todo.contents.description}</p>
     <hr/>
   </>
+  }else if(todo.state=="hasError"){
+    <b>There is some server error...</b>
+  }
 }
